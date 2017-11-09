d7e5d85ece5033ec8ae708c3927ac090eb375351
Ben Wong <benwong0@live.com>
2017-11-08 17:56:10 -0800

    dispatch_semaphore_wait bug fix

--- Rollbar/RollbarNotifier.m
+++ Rollbar/RollbarNotifier.m
@@ -344,7 +344,7 @@ static BOOL isNetworkReachable = YES;
             dispatch_semaphore_signal(sem);
         }];
         [dataTask resume];
-        dispatch_semaphore_wait(sem, dispatch_time(DISPATCH_TIME_NOW, DISPATCH_TIME_FOREVER));
+        dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
     } else {
         // Using method sendSynchronousRequest, deprecated since iOS 9.0
         NSError *error;
