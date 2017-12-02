cd7a87323bbf8f8a0c27089f49c9f308d56507a6
Ben Wong <benwong0@live.com>
2017-12-02 01:40:37 -0800

    Adding log unit test

--- RollbarTests/RollbarTests.m
+++ RollbarTests/RollbarTests.m
@@ -2,11 +2,13 @@
 //  RollbarTests.m
 //  RollbarTests
 //
-//  Created by Sergei Bezborodko on 3/18/14.
-//  Copyright (c) 2014 Rollbar. All rights reserved.
+//  Created by Ben Wong on 11/30/17.
+//  Copyright © 2017 Rollbar. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "Rollbar.h"
+#import "RollbarTestUtil.h"
 
 @interface RollbarTests : XCTestCase
 
@@ -14,21 +16,45 @@
 
 @implementation RollbarTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    RollbarClearLogFile();
+    [Rollbar initWithAccessToken:@""];
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown {
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testNotification {
+    NSDictionary *notificationText = @{
+                                       @"debug": @[@"testing-debug"],
+                                       @"error": @[@"testing-error"],
+                                       @"error": @[@"testing-error-with-message", [NSException exceptionWithName:@"testing-error" reason:@"testing-error-2" userInfo:nil]],
+                                       @"info": @[@"testing-info"],
+                                       @"critical": @[@"testing-critical"],
+                                       };
+    NSString *text = @"testing";
+    for (NSString *type in notificationText.allKeys) {
+        NSArray *params = notificationText[type];
+        if ([type isEqualToString:@"error"]) {
+            if (params.count == 2) {
+                [Rollbar error:params[0] exception:params[1]];
+            } else {
+                [Rollbar error:params[0]];
+            }
+        } else if ([type isEqualToString:@"debug"]) {
+            [Rollbar debug:params[0]];
+        } else if ([type isEqualToString:@"info"]) {
+            [Rollbar info:params[0]];
+        } else if ([type isEqualToString:@"critical"]) {
+            [Rollbar critical:params[0]];
+        }
+    }
+    [Rollbar error:text];
+    NSArray *items = RollbarReadLogItemFromFile();
+    for (id itm in items) {
+    }
 }
 
 @end
