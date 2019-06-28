8c452806d70ff261edc7fbcd92bb8fd99e07dabc
Andrey Kornich <akornich@gmail.com>
2019-06-28 11:21:37 -0700

    feat: added static libs ios and macos targets

---
+++ Rollbar-macOS/Rollbar_macOS.h
@@ -0,0 +1,13 @@
+//
+//  Rollbar_macOS.h
+//  Rollbar-macOS
+//
+//  Created by Andrey Kornich on 2019-06-28.
+//  Copyright © 2019 Rollbar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rollbar_macOS : NSObject
+
+@end
