bd77c260acf7458a53b08415cad6030c39eff583
Andrey Kornich <akornich@gmail.com>
2019-07-08 16:43:58 -0700

    docs: ref #175: Create a sample app - macOSAppWithRollbarCocoaPod.

---
+++ Examples/macOSAppWithRollbarCocoaPod/macOSAppWithRollbarCocoaPod/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  macOSAppWithRollbarCocoaPod
+//
+//  Created by Andrey Kornich on 2019-07-08.
+//  Copyright © 2019 Andrey Kornich (Wide Spectrum Computing LLC). All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
