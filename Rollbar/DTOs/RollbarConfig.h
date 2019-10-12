1d6c368a422be8c700438fb522525a292437ec40
Andrey Kornich <akornich@gmail.com>
2019-10-11 23:56:50 -0700

    feat: ref #194: Capture relevant notifier config with every payload.
    - added RollbarConfig DTO stub

---
+++ Rollbar/DTOs/RollbarConfig.h
@@ -0,0 +1,17 @@
+//
+//  RollbarConfig.h
+//  Rollbar
+//
+//  Created by Andrey Kornich on 2019-10-11.
+//  Copyright © 2019 Rollbar. All rights reserved.
+//
+
+#import "DataTransferObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RollbarConfig : DataTransferObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Rollbar/DTOs/RollbarConfig.m
@@ -0,0 +1,13 @@
+//
+//  RollbarConfig.m
+//  Rollbar
+//
+//  Created by Andrey Kornich on 2019-10-11.
+//  Copyright © 2019 Rollbar. All rights reserved.
+//
+
+#import "RollbarConfig.h"
+
+@implementation RollbarConfig
+
+@end
