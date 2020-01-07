6bdda938ccc0d4617b1cc50db02c2959cc9a2284
Andrey Kornich <akornich@gmail.com>
2020-01-06 19:59:09 -0800

    chore: bumped the version up

--- Rollbar/DTOs/RollbarConfig.m
+++ Rollbar/DTOs/RollbarConfig.m
@@ -23,7 +23,7 @@
 
 #pragma mark - constants
 
-static NSString * const NOTIFIER_VERSION = @"1.11.1";
+static NSString * const NOTIFIER_VERSION = @"1.11.2";
 
 #define NOTIFIER_NAME_PREFIX = @"rollbar-";
 #if TARGET_OS_IPHONE
