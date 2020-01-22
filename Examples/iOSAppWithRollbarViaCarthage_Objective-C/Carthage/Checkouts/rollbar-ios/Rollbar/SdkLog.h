9a7788201c7cab598d15767549e886d7e1768bda
Andrey Kornich <akornich@gmail.com>
2020-01-22 15:34:30 -0800

    chore: resolve #241
    Clear unneeded Build and Checkout folders content from iOSAppWithRollbarViaCarthage_Objective-C/Carthage

--- Examples/iOSAppWithRollbarViaCarthage_Objective-C/Carthage/Checkouts/rollbar-ios/Rollbar/SdkLog.h
+++
@@ -1,8 +0,0 @@
-//  Copyright (c) 2018 Rollbar Inc. All rights reserved.
-
-#import <Foundation/Foundation.h>
-
-/// SDK-wide logging function.
-/// Use it for all the SDK development/debugging needs.
-/// @param format logged message format
-void SdkLog(NSString *format, ...);
