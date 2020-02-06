03c4afc61bcb81bbf6f7519d02bbd509741c5919
Andrey Kornich <akornich@gmail.com>
2020-02-06 13:14:43 -0800

    feat: ref #212: Add Swift Package Manager support.

--- Package.swift
+++ Package.swift
@@ -2,11 +2,17 @@
 import PackageDescription
 
 let package = Package(name: "Rollbar",
-                      platforms: [.macOS(.v10_12),
-                                  .iOS(.v8)//,
-                                  //.tvOS(.v10),
-                                  //.watchOS(.v3)
+                      platforms: [
+                        .macOS(.v10_12),
+                        .iOS(.v8)//,
+                         //.tvOS(.v10),
+                         //.watchOS(.v3)
                         ],
-                      products: [.library(name: "Rollbar", targets: ["Rollbar-SPM"])],
-                      targets: [.target(name: "Rollbar-SPM", path: "Rollbar")]
+                      products: [
+                        .library(name: "Rollbar", targets: ["Rollbar-SPM"])
+                        ],
+                      targets: [
+                        .target(name: "KSCrash", path: "KSCrash"),
+                        .target(name: "Rollbar-SPM", path: "Rollbar", dependencies: ["KSCrash"]),
+                        ]
 )
