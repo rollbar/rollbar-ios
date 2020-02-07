6a7ac1d557c757965fba2e129d1a39f16b7ef86b
Andrey Kornich <akornich@gmail.com>
2020-02-06 17:08:35 -0800

    feat: ref #212: Add Swift Package Manager support.

---
+++ Examples/macOSAppWithSPM/macOSAppWithSPM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOSAppWithSPM
+//
+//  Created by Andrey Kornich on 2020-02-06.
+//  Copyright © 2020 Andrey Kornich (Wide Spectrum Computing LLC). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
