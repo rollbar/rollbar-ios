59d64b57f52ec5b6097fd5f3b2e9147325053487
Andrey Kornich <akornich@gmail.com>
2020-03-04 18:16:31 -0800

    eat: ref #261: Turn telemetry events into DTOs

--- Rollbar/Notifier_DTOs/RollbarTelemetryErrorBody.h
+++ Rollbar/Notifier_DTOs/RollbarTelemetryErrorBody.h
@@ -8,11 +8,32 @@
 
 #import <Foundation/Foundation.h>
 
-#import "RollbarTelemetryBody.h"
+#import "RollbarTelemetryLogBody.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface RollbarTelemetryErrorBody : RollbarTelemetryBody
+@interface RollbarTelemetryErrorBody : RollbarTelemetryLogBody
+
+//#pragma mark - Properties
+//
+//@property (nonatomic, copy) NSString *message;
+//
+//#pragma mark - Initializers
+//
+//-(instancetype)initWithMessage:(nonnull NSString *)message
+//                     extraData:(nullable NSDictionary *)extraData
+//NS_DESIGNATED_INITIALIZER;
+//
+//-(instancetype)initWithMessage:(nonnull NSString *)message;
+//
+//- (instancetype)initWithArray:(NSArray *)data
+//NS_DESIGNATED_INITIALIZER;
+//
+//- (instancetype)initWithDictionary:(NSDictionary *)data
+//NS_DESIGNATED_INITIALIZER;
+//
+//- (instancetype)init
+//NS_UNAVAILABLE;
 
 @end
 
--- Rollbar/Notifier_DTOs/RollbarTelemetryNavigationBody.h
+++ Rollbar/Notifier_DTOs/RollbarTelemetryNavigationBody.h
@@ -14,6 +14,30 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RollbarTelemetryNavigationBody : RollbarTelemetryBody
 
+#pragma mark - Properties
+
+@property (nonatomic, copy) NSString *from;
+@property (nonatomic, copy) NSString *to;
+
+#pragma mark - Initializers
+
+-(instancetype)initWithFromLocation:(nonnull NSString *)from
+                         toLocation:(nonnull NSString *)to
+                          extraData:(nullable NSDictionary *)extraData
+NS_DESIGNATED_INITIALIZER;
+
+-(instancetype)initWithFromLocation:(nonnull NSString *)from
+                         toLocation:(nonnull NSString *)to;
+
+- (instancetype)initWithArray:(NSArray *)data
+NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithDictionary:(NSDictionary *)data
+NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)init
+NS_UNAVAILABLE;
+
 @end
 
 NS_ASSUME_NONNULL_END
