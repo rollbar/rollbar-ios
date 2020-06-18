82bf003ad5399bfbb6c7b0d79a82570319327311
Andrey Kornich <akornich@gmail.com>
2020-06-17 18:27:41 -0700

    added macosAppSwiftCocoapods demo sample

---
+++ Demos/macosAppSwiftCocoapods/macosAppSwiftCocoapods/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macosAppSwiftCocoapods
+//
+//  Created by Andrey Kornich on 2020-06-17.
+//  Copyright © 2020 Andrey Kornich (Wide Spectrum Computing LLC). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
