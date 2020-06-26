efe0fd6c3e722acf9b7db4c72dc252eed1a39371
Andrey Kornich <akornich@gmail.com>
2020-06-25 17:45:16 -0700

    added macosAppSwiftSPM demo app
    - initial clean app project

---
+++ Demos/macosAppSwiftSPM/macosAppSwiftSPM/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macosAppSwiftSPM
+//
+//  Created by Andrey Kornich on 2020-06-25.
+//  Copyright © 2020 Andrey Kornich (Wide Spectrum Computing LLC). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
