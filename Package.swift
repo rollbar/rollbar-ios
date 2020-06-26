52f6577ea17a41e0aaa0af92d08edfffcb401ad0
Andrey Kornich <akornich@gmail.com>
2020-06-25 21:36:32 -0700

    fixing SDK level SPM package manifest

--- Package.swift
+++ Package.swift
@@ -24,11 +24,11 @@ let package = Package(
             name: "RollbarNotifier",
             targets: ["RollbarNotifier"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "RollbarCommon"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        // .package(url: /* package url */, from: "1.0.0"),
+//        .package(path: "RollbarCommon"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
