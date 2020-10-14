50d20848f9a49ff5d18e21dc2140b3251988e3c0
Andrey Kornich <akornich@gmail.com>
2020-10-14 00:04:52 -0700

    chore: code cleanup related to the testRollbarTaskDispatcher unit test

--- RollbarCommon/Tests/RollbarCommonTests-ObjC/RollbarCommonTests.m
+++ RollbarCommon/Tests/RollbarCommonTests-ObjC/RollbarCommonTests.m
@@ -6,103 +6,13 @@
 //  Copyright © 2019 Rollbar. All rights reserved.
 //
 
-@import Foundation;
 
 #if !TARGET_OS_WATCH
 #import <XCTest/XCTest.h>
 
+@import Foundation;
 @import RollbarCommon;
-
-typedef NS_ENUM(NSUInteger, CalculatorOperation) {
-    CalculatorOperation_Add,
-    CalculatorOperation_Subtract,
-    CalculatorOperation_Multiply,
-    CalculatorOperation_Divide,
-};
-
-@interface Calculator : NSObject {
-    @private
-    XCTestExpectation *_expectation;
-}
-
-@property (readonly) NSInteger operand1;
-@property (readonly) NSInteger operand2;
-@property (readonly) CalculatorOperation operation;
-
--(NSInteger)calculate;
-
--(instancetype)init NS_UNAVAILABLE;
-
--(instancetype)initWithOperation:(CalculatorOperation)operation
-                        operand1:(NSInteger)operand1
-                        operand2:(NSInteger)operand2
-                     expectation:(XCTestExpectation *)expectation
-NS_DESIGNATED_INITIALIZER;
-
-@end
-
-@implementation Calculator
-
--(NSInteger)calculate {
-
-    NSString *operator;
-    switch(_operation) {
-        case CalculatorOperation_Add:
-            operator = @"+";
-            break;
-        case CalculatorOperation_Subtract:
-            operator = @"-";
-            break;
-        case CalculatorOperation_Multiply:
-            operator = @"*";
-            break;
-        case CalculatorOperation_Divide:
-            operator = @"/";
-            break;
-    }
-    NSLog(@"Calculating: %li %@ %li", (long)_operand1, operator, (long)_operand2);
-    
-    NSInteger result;
-    switch(_operation) {
-        case CalculatorOperation_Add:
-            result = _operand1 + _operand2;
-            break;
-        case CalculatorOperation_Subtract:
-            result = _operand1 - _operand2;
-            break;
-        case CalculatorOperation_Multiply:
-            result = _operand1 * _operand2;
-            break;
-        case CalculatorOperation_Divide:
-            result = _operand1 / _operand2;
-            break;
-    }
-    
-    [_expectation fulfill];
-    NSLog(@"Calculated: %li %@ %li. Result: %li", (long)_operand1, operator, (long)_operand2, (long)result);
-
-    return result;
-}
-
--(instancetype)initWithOperation:(CalculatorOperation)operation
-                        operand1:(NSInteger)operand1
-                        operand2:(NSInteger)operand2
-                     expectation:(XCTestExpectation *)expectation {
-    
-    self = [super init];
-    if (self == nil) {
-        return nil;
-    }
-    
-    _operation = operation;
-    _operand1 = operand1;
-    _operand2 = operand2;
-    _expectation = expectation;
-    
-    return  self;
-}
-@end
-
+#import "Mocks/Calculator.h"
 
 @interface RollbarCommonTests : XCTestCase
 
